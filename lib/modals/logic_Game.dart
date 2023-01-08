47fd471316cca057c232094ba23691231c22e607
Taher_Elzoghby <tataamen678@gmail.com>
2023-01-08 15:40:33 +0200

    autoPlay

--- lib/modals/logic_Game.dart
+++ lib/modals/logic_Game.dart
@@ -1,9 +1,17 @@
+import 'dart:math';
+
 class Player {
   static const x = "X";
   static const o = "O";
   static const empty = "";
   static List<int> playerX = []; //0 2 4 6
   static List<int> playerO = []; //1 3 5 8 9
+}
+
+extension ContainsAll on List {
+  bool containsAll(int x, int y, int z) {
+    return contains(x) && contains(y) && contains(z);
+  }
 }
 
 class Game {
@@ -15,6 +23,42 @@ class Game {
     }
   }
 
-  checkWinner() {}
-  Future autoPlay() async {}
+  checkWinner() {
+    String winner = "";
+    if (Player.playerX.containsAll(0, 1, 2) ||
+        Player.playerX.containsAll(3, 4, 5) ||
+        Player.playerX.containsAll(6, 7, 8) ||
+        Player.playerX.containsAll(0, 3, 6) ||
+        Player.playerX.containsAll(1, 4, 7) ||
+        Player.playerX.containsAll(2, 5, 8) ||
+        Player.playerX.containsAll(0, 4, 8) ||
+        Player.playerX.containsAll(2, 4, 6)) {
+      winner = "X";
+    } else if (Player.playerX.containsAll(0, 1, 2) ||
+        Player.playerO.containsAll(3, 4, 5) ||
+        Player.playerO.containsAll(6, 7, 8) ||
+        Player.playerO.containsAll(0, 3, 6) ||
+        Player.playerO.containsAll(1, 4, 7) ||
+        Player.playerO.containsAll(2, 5, 8) ||
+        Player.playerO.containsAll(0, 4, 8) ||
+        Player.playerO.containsAll(2, 4, 6)) {
+      winner = "O";
+    } else {
+      winner = "";
+    }
+  }
+
+  Future autoPlay(String activePlayer) async {
+    int index = 0;
+    List<int> emptyL = [];
+    for (int i = 0; i < 9; i++) {
+      if (!(Player.playerO.contains(i) || Player.playerX.contains(i))) {
+        emptyL.add(i);
+      }
+    }
+    Random randomInd = Random();
+    int rInd = randomInd.nextInt(emptyL.length);
+    index = emptyL[rInd];
+    playGame(index, activePlayer);
+  }
 }
