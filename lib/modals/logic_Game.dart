9806f9a203b87e3ba3e5fc859a751467fc099142
Taher_Elzoghby <tataamen678@gmail.com>
2023-01-08 18:26:33 +0200

    edit auto play smart

--- lib/modals/logic_Game.dart
+++ lib/modals/logic_Game.dart
@@ -61,6 +61,7 @@ class Game {
         emptyL.add(i);
       }
     }
+
     //start-center
     if (Player.playerO.containsAll(0, 1) && emptyL.contains(2)) {
       index = 2;
@@ -80,8 +81,8 @@ class Game {
       index = 6;
     }
     //center-end
-    if (Player.playerO.containsAll(1, 2) && emptyL.contains(0)) {
-      index = 1;
+    else if (Player.playerO.containsAll(1, 2) && emptyL.contains(0)) {
+      index = 0;
     } else if (Player.playerO.containsAll(4, 5) && emptyL.contains(3)) {
       index = 3;
     } else if (Player.playerO.containsAll(7, 8) && emptyL.contains(6)) {
@@ -98,7 +99,7 @@ class Game {
       index = 2;
     }
     //start-end
-    if (Player.playerO.containsAll(0, 2) && emptyL.contains(1)) {
+    else if (Player.playerO.containsAll(0, 2) && emptyL.contains(1)) {
       index = 1;
     } else if (Player.playerO.containsAll(3, 5) && emptyL.contains(4)) {
       index = 4;
@@ -115,9 +116,64 @@ class Game {
     } else if (Player.playerO.containsAll(2, 6) && emptyL.contains(4)) {
       index = 4;
     }
-    Random randomInd = Random();
-    int rInd = randomInd.nextInt(emptyL.length);
-    index = emptyL[rInd];
+    if (Player.playerX.containsAll(0, 1) && emptyL.contains(2)) {
+      index = 2;
+    } else if (Player.playerX.containsAll(3, 4) && emptyL.contains(5)) {
+      index = 5;
+    } else if (Player.playerX.containsAll(6, 7) && emptyL.contains(8)) {
+      index = 8;
+    } else if (Player.playerX.containsAll(0, 3) && emptyL.contains(6)) {
+      index = 6;
+    } else if (Player.playerX.containsAll(1, 4) && emptyL.contains(7)) {
+      index = 7;
+    } else if (Player.playerX.containsAll(2, 5) && emptyL.contains(8)) {
+      index = 8;
+    } else if (Player.playerX.containsAll(0, 4) && emptyL.contains(8)) {
+      index = 8;
+    } else if (Player.playerX.containsAll(2, 4) && emptyL.contains(6)) {
+      index = 6;
+    }
+    //center-end
+    else if (Player.playerX.containsAll(1, 2) && emptyL.contains(0)) {
+      index = 0;
+    } else if (Player.playerX.containsAll(4, 5) && emptyL.contains(3)) {
+      index = 3;
+    } else if (Player.playerX.containsAll(7, 8) && emptyL.contains(6)) {
+      index = 6;
+    } else if (Player.playerX.containsAll(3, 6) && emptyL.contains(0)) {
+      index = 0;
+    } else if (Player.playerX.containsAll(4, 7) && emptyL.contains(1)) {
+      index = 1;
+    } else if (Player.playerX.containsAll(5, 8) && emptyL.contains(2)) {
+      index = 2;
+    } else if (Player.playerX.containsAll(4, 8) && emptyL.contains(0)) {
+      index = 0;
+    } else if (Player.playerX.containsAll(4, 6) && emptyL.contains(2)) {
+      index = 2;
+    }
+    //start-end
+    else if (Player.playerX.containsAll(0, 2) && emptyL.contains(1)) {
+      index = 1;
+    } else if (Player.playerX.containsAll(3, 5) && emptyL.contains(4)) {
+      index = 4;
+    } else if (Player.playerX.containsAll(6, 8) && emptyL.contains(7)) {
+      index = 7;
+    } else if (Player.playerX.containsAll(0, 6) && emptyL.contains(3)) {
+      index = 3;
+    } else if (Player.playerX.containsAll(1, 7) && emptyL.contains(4)) {
+      index = 4;
+    } else if (Player.playerX.containsAll(2, 8) && emptyL.contains(5)) {
+      index = 5;
+    } else if (Player.playerX.containsAll(0, 8) && emptyL.contains(4)) {
+      index = 4;
+    } else if (Player.playerX.containsAll(2, 6) && emptyL.contains(4)) {
+      index = 4;
+    } else {
+      Random randomInd = Random();
+      int rInd = randomInd.nextInt(emptyL.length);
+      index = emptyL[rInd];
+    }
+
     playGame(index, activePlayer);
   }
 }
