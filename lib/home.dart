d02227e7d85e299dafb79622db45079aea421ca3
Taher_Elzoghby <tataamen678@gmail.com>
2023-01-18 09:54:45 +0200

    editited

--- lib/home.dart
+++ lib/home.dart
@@ -11,69 +11,114 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     final wat = context.watch<contr>();
     final rea = context.read<contr>();
+    bool landScape =
+        MediaQuery.of(context).orientation == Orientation.landscape;
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            //title
-            Text('TIC TAC TOE', style: Theme.of(context).textTheme.headline2),
-            //sitched on /off two player
-            SwitchListTile(
-              title: Text(
-                'Turn on/off two player',
-                style: Theme.of(context).textTheme.headline5,
-              ),
-              value: wat.isSwitched,
-              onChanged: (v) => rea.changeSwitched(v),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            //board
-            Expanded(
-              child: GridView.count(
-                padding: const EdgeInsets.all(20),
-                crossAxisCount: 3,
-                mainAxisSpacing: 5,
-                crossAxisSpacing: 5,
-                children: List.generate(
-                  9,
-                  (index) => InkWell(
-                    borderRadius: BorderRadius.circular(40),
-                    onTap: wat.gameOver ? null : () => rea.onTapIndex(index),
-                    child: containerBoard(
-                      index: index,
-                    ),
+        child: !landScape
+            ? Column(
+                children: [
+                  //title
+                  ...firstBlock(context, wat, rea),
+                  SizedBox(
+                    height: landScape == true ? 5 : 20,
                   ),
-                ),
+                  //board
+                  _expanded(
+                    landScape: landScape,
+                    rea: rea,
+                    wat: wat,
+                  ),
+                  //active player now
+                  ...lastBlock(context, wat, rea),
+                  SizedBox(
+                    height: landScape == true
+                        ? MediaQuery.of(context).size.height * .05
+                        : MediaQuery.of(context).size.height * .1,
+                  ),
+                ],
+              )
+            : Row(
+                children: [
+                  ...firstBlock(context, wat, rea),
+                  ...lastBlock(context, wat, rea),
+                  _expanded(landScape: landScape, wat: wat, rea: rea),
+                ],
               ),
+      ),
+    );
+  }
+
+  List<Widget> firstBlock(context, wat, rea) {
+    return [
+      Text('TIC TAC TOE', style: Theme.of(context).textTheme.headline2),
+      //sitched on /off two player
+      SwitchListTile(
+        title: Text(
+          'Turn on/off two player',
+          style: Theme.of(context).textTheme.headline5,
+        ),
+        value: wat.isSwitched,
+        onChanged: (v) => rea.changeSwitched(v),
+      ),
+    ];
+  }
+
+  List<Widget> lastBlock(context, wat, rea) {
+    return [
+      Text(
+        'It\'s ${wat.activePlayer} player',
+        style: Theme.of(context).textTheme.headline3,
+      ),
+      //result
+      Text(
+        wat.result,
+        style: Theme.of(context).textTheme.headline4,
+      ),
+      //repeat the game
+      ElevatedButton.icon(
+        onPressed: () => rea.repeatTheGame(),
+        icon: const Icon(Icons.repeat),
+        label: const Text(
+          'Repeat the game',
+        ),
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(Colors.lightBlueAccent),
+        ),
+      ),
+    ];
+  }
+}
+
+class _expanded extends StatelessWidget {
+  const _expanded({
+    Key? key,
+    required this.landScape,
+    required this.wat,
+    required this.rea,
+  }) : super(key: key);
+
+  final bool landScape;
+  final contr wat;
+  final contr rea;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: GridView.count(
+        padding: const EdgeInsets.all(20),
+        crossAxisCount: 3,
+        mainAxisSpacing: 5,
+        crossAxisSpacing: 5,
+        children: List.generate(
+          9,
+          (index) => InkWell(
+            borderRadius: BorderRadius.circular(40),
+            onTap: wat.gameOver ? null : () => rea.onTapIndex(index),
+            child: containerBoard(
+              index: index,
             ),
-            //active player now
-            Text(
-              'It\'s ${wat.activePlayer} player',
-              style: Theme.of(context).textTheme.headline3,
-            ),
-            //result
-            Text(
-              wat.result,
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            //repeat the game
-            ElevatedButton.icon(
-              onPressed: () => rea.repeatTheGame(),
-              icon: const Icon(Icons.repeat),
-              label: const Text(
-                'Repeat the game',
-              ),
-              style: ButtonStyle(
-                backgroundColor:
-                    MaterialStateProperty.all(Colors.lightBlueAccent),
-              ),
-            ),
-            const SizedBox(
-              height: 100,
-            ),
-          ],
+          ),
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:tictac/controls/controllerr.dart';
 
 import 'home.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitDown,
+    DeviceOrientation.portraitUp,
+  ]);
   runApp(ChangeNotifierProvider<contr>(
     create: (_) => contr(),
     child: const MyApp(),
